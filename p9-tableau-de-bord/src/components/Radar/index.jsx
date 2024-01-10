8fca7d3d6cec6b8753abe6b446811b9bf3ad8cd9
leo23225 <leo_94310@hotmail.com>
2024-01-10 13:49:04 +0100

    fix

--- p9-tableau-de-bord/src/components/Radar/index.jsx
+++ p9-tableau-de-bord/src/components/Radar/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
+import fetchPerformanceData from '../../services/performanceService'
 
 const dataKeyValue = (subject) => {
   switch (subject) {
@@ -20,37 +21,36 @@ const dataKeyValue = (subject) => {
   }
 };
 
-
 function RadarRender() {
-  const [performanceData, setPerformanceData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [rechartsArray, setRechartsArray] = useState(null);
 
   useEffect(() => {
-    
+    const userId = window.location.pathname.split('/').pop();
     // Appel du service pour obtenir les données utilisateur
     fetchPerformanceData(userId)
-      .then((data) => setPerformanceData(data))
-      .catch((error) => console.error('Erreur lors de la récupération des données utilisateur :', error));
-    }, []);
+    
+      .then((data) => {
+        const kindArray = data.kind;
+        const dataArray = data.data;
 
-    if (performanceData && performanceData.data) {
-      const kindArray = performanceData.data.kind;
-      const dataArray = performanceData.data.data;
-  
-      // Définir l'ordre souhaité
-      const order = ['intensity', 'speed', 'strength', 'endurance', 'energy', 'cardio'];
-  
-      // Filtrer et trier les données selon l'ordre défini
-      const filteredAndSortedData = order.map(subject => {
-        const dataEntry = dataArray.find(entry => kindArray[entry.kind] === subject);
-        return { subject: dataKeyValue(subject), A: dataEntry ? dataEntry.value : 0 };
-      });
-  
-      setRechartsArray(filteredAndSortedData);
-    }
-  }, [];
+        // Définir l'ordre souhaité
+        const order = ['intensity', 'speed', 'strength', 'endurance', 'energy', 'cardio'];
+
+        // Filtrer et trier les données selon l'ordre défini
+        const filteredAndSortedData = order.map(subject => {
+          const dataEntry = dataArray.find(entry => kindArray[entry.kind] === subject);
+          return { subject: dataKeyValue(subject), A: dataEntry ? dataEntry.value : 0 };
+        });
+        setLoading(false);
+        setRechartsArray(filteredAndSortedData);
+      })
+      .catch((error) => 
+        console.error('Erreur lors de la récupération des données utilisateur :', error));
+        setError(error.message);
+        setLoading(false);
+  }, []);
 
   return (
     <section id='Radar' className='Stats_section'>
